bf6ac985250d41f90db5a680c03dfceff6eed597
Olivier Mailhot <fermi@mailhot-iric.local>
2026-06-06 11:54:17 -0400

    fps

--- get_chembl_distance_narval.py
+++ get_chembl_distance_narval.py
@@ -10,7 +10,7 @@ def make_chembl_smi():
     cids = [x for x in chembl_df["ChEMBL ID"].values]
     smiles_list = [x for x in chembl_df["Smiles"].values]
     for smiles, cid in zip(smiles_list, cids):
-        if 'nan' in smiles:
+        if 'nan' in str(smiles):
             continue
         fout.write(f"{smiles} {cid}\n")
     fout.close()
