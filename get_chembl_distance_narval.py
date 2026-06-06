6dd92fc16fbe6e29111b848606bd149918d79bad
Olivier Mailhot <fermi@mailhot-iric.local>
2026-06-06 08:00:16 -0400

    first commit

---
+++ get_chembl_distance_narval.py
@@ -0,0 +1,10 @@
+from omltk.fingerprints import get_fp_from_smiles, get_tanimoto_array
+import numpy as np
+from multiprocessing import Pool
+import pandas as pd
+
+
+
+if __name__ == "__main__":
+    chembl_df = pd.read_csv("/home/mailhoto/projects/rrg-mailhoto/share/chembl36/all_chembl36_part1.csv")
+
